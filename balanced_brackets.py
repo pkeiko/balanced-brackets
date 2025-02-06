69fcae3ce6af2c67cb21114617722530c14b32b5
pkeiko <paula.keiko.m@gmail.com>
2025-02-05 23:53:17 -0300

    fix: changes output file name

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -33,7 +33,7 @@ def isBalanced(s):
     return 'NO'
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open('output.txt', 'w')
 
     t = int(input().strip())
 
